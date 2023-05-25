8cb6220dcaf203b2e95241754219c7b7bb162ea9
bryanskinner <bryanskinner99@gmail.com>
2023-05-25 02:58:47 -0500

    Finished Node Practice and did the  section as well

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,30 @@
 const isEven = (num) => {
   // write code for numbers.isEven
+  return num % 2 ===  0;
 
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  let total = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+  total += arr[i];
+  }
+  return total;
 }
+
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === sum) {
+      return true;
+      }
+    }
+  }
+  return false;
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,23 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  return str.split(delim);
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  const pairs = [];
 
+  for (let i = 0; i < str.length; i += 2) {
+    const pair = str.slice(i, i + 2);
+    pairs.push(pair);
+  }
+
+  return pairs;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+return str.split(``).reverse().join(``);
 }
 
 module.exports = {
